176ddb80d87c61c0a7e4a708fe7d29fe121ebc20
Ruslan Valiev <40303641+gogiasss@users.noreply.github.com>
2020-05-31 00:04:05 +0500

    update comments

--- lottery.py
+++ lottery.py
@@ -5,14 +5,16 @@ lotts = int(input("Введите КОНЕЧНОЕ число в лотерее,
 nums = int(input("Введите сколько цифр отметить (например 5 или 6: "))
 count = int(input("Введите количество комбинаций: "))
 
-
+# Создаем список
 for i in range(lotts+1):
     numbers.append(i)
     
 numbers.remove(0) #удаляем 0
-     
+
+# Выбираем случайные цифры     
 def lottery():
     print(sample(numbers,nums))
- 
+
+# Генерируем количество комбинаций 
 for i in range(count):
     print(lottery())

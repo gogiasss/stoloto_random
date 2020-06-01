48158e8cc81eacaf505adf94c6f07b28aa877fcd
Rus <valiev.ruslan@gmail.com>
2020-06-01 10:08:07 +0500

    update new

---
+++ lottery_gen.py
@@ -0,0 +1,27 @@
+import random
+
+numbers = []
+lotts = int(input("Введите КОНЕЧНОЕ число в лотерее, например 36 или 45: "))
+nums = int(input("Введите сколько цифр отметить (например 5 или 6: "))
+#count = int(input("Введите количество комбинаций: "))
+
+print(numbers)
+
+numbers = [random.randint(1, lotts) for i in range(nums)]
+
+print(numbers)
+
+
+### Создаем список
+##for i in range(lotts+1):
+##    numbers.append(i)
+##    
+##numbers.remove(0) #удаляем 0
+##
+### Выбираем случайные цифры     
+##def lottery():
+##    print(sample(numbers,nums)) 
+##
+### Генерируем количество комбинаций 
+##for i in range(count):
+##    print(lottery())
